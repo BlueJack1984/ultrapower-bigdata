a74f83d9321c70a2cc269bae896b78750988005b
lushusheng <lushushen8@163.com>
2020-01-10 11:40:11 +0800

    commit java files by idea by daniel

--- bigdata-admin/src/main/java/com/example/admin/dto/request/UserInput.java
+++ bigdata-admin/src/main/java/com/example/admin/dto/request/UserInput.java
@@ -15,9 +15,37 @@ import java.io.Serializable;
 public class UserInput implements Serializable {
 
     /**
+     * 用户类型：企业用户，数据审核员，管理员
+     */
+    private Integer targetType;
+    /**
      * 账号
      */
     @Length()
     @NotBlank(message = "")
     private String account;
+    /**
+     * 真实姓名
+     */
+    private String realName;
+    /**
+     * 公司名称
+     */
+    private String corporationName;
+    /**
+     * 公司职位
+     */
+    private String position;
+    /**
+     * 邮箱
+     */
+    private String email;
+    /**
+     * 邮箱
+     */
+    private String url;
+    /**
+     * 邮箱
+     */
+    private String surl;
 }
