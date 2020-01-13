cd0464d82778256fa4fbeed12b7e01326cbed96f
lushusheng <lushushen8@163.com>
2020-01-13 21:54:04 +0800

    commit java files by idea by daniel

--- bigdata-admin/src/main/java/com/example/admin/config/fileupload/FileUploadConfig.java
+++ bigdata-admin/src/main/java/com/example/admin/config/fileupload/FileUploadConfig.java
@@ -3,6 +3,7 @@ package com.example.admin.config.fileupload;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
 
 import javax.servlet.MultipartConfigElement;
 
@@ -15,17 +16,16 @@ import javax.servlet.MultipartConfigElement;
 public class FileUploadConfig {
 
     /**
-     *
+     * 设置上传文件的大小
      * @return
      */
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
-        //单个文件最大   KB,MB
-        //factory.setMaxFileSize("100MB");
-        //factory.set
-        /// 设置总上传数据总大小
-        //factory.setMaxRequestSize("500MB");
+        //单个文件最大100 * 1024KB,100MB
+        factory.setMaxFileSize(DataSize.ofKilobytes(100 * 1024));
+        /// 设置总上传数据总大小500 * 1024KB,500MB
+        factory.setMaxRequestSize(DataSize.ofKilobytes(500 * 1024));
         return factory.createMultipartConfig();
     }
 }
